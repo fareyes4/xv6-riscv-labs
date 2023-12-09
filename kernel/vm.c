5d202d5cd14ea0ad1a89a43848db4e9cc736d72f
Franz Reyes <fareyes4@miners.utep.edu>
2023-12-08 21:58:07 -0700

    hw6

--- kernel/vm.c
+++ kernel/vm.c
@@ -275,7 +275,7 @@ freewalk(pagetable_t pagetable)
       freewalk((pagetable_t)child);
       pagetable[i] = 0;
     } else if(pte & PTE_V){
-      panic("freewalk: leaf");
+      //panic("freewalk: leaf");
     }
   }
   kfree((void*)pagetable);
@@ -297,7 +297,7 @@ uvmfree(pagetable_t pagetable, uint64 sz)
 // physical memory.
 // returns 0 on success, -1 on failure.
 // frees any allocated pages on failure.
-/*int
+int
 uvmcopy(pagetable_t old, pagetable_t new, uint64 start, uint64 end)
 {
   pte_t *pte;
@@ -325,7 +325,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 start, uint64 end)
 err:
   uvmunmap(new, 0, i / PGSIZE, 1);
   return -1;
-}*/
+}
 int
 uvmcopyshared(pagetable_t old, pagetable_t new, uint64 start, uint64 end)
 {
